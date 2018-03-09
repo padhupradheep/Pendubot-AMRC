eac91759f5f96282ea23a518283a17a24966c2ea
padhupradheep <padhupradheep@gmail.com>
2018-03-09 18:33:01 +0530

    Linearization is done - now have to substitute the values

--- dynamics_pendubot.m
+++ dynamics_pendubot.m
@@ -8,10 +8,11 @@ syms m1 m2 l_1 l_2 q_1 q_2 q_1d q_2d; %Pendubot's mass of the links, length of t
 syms tau_1 tau_2; %Torque
 syms g; 
 syms q_1d q_1dd q_2d q_2dd;
+syms v_1;
 
 % Step 2. Now computing the position and velocities of the masses in the
 % system:
-
+y = [q_1;q_2];
 p_q_1 = l_1*cos(q_1);  
 p_y1 = l_1*sin(q_1);
 
@@ -37,7 +38,7 @@ PE = m1*g*p_y1 +m2*g*p_y2;
 KE = simplify(KE);
 PE = simplify(PE);
 L = KE-PE; 
-
+matlabFunction(L, 'file', 'Lagrange');
 %Deriving the equaations of motion from Lagrangian: 
 
 pKEpq_1d = diff(KE,q_1d);
@@ -48,7 +49,7 @@ ddtpKEpq_1d = diff(pKEpq_1d,q_1)*q_1d+ ...
 pKEpq_1 = diff(KE,q_1);
 pPEpq_1 = diff(PE,q_1);
 
-
+%Using chain rule in order to derive the lagrange mechanics
 pKEpq_2d = diff(KE,q_2d);
 ddtpKEpq_2d = diff(pKEpq_2d,q_1)*q_1d+ ...
              diff(pKEpq_2d,q_1d)*q_1dd+ ...
@@ -57,5 +58,13 @@ ddtpKEpq_2d = diff(pKEpq_2d,q_1)*q_1d+ ...
 pKEpq_2 = diff(KE,q_2);
 pPEpq_2 = diff(PE,q_2);   
 
-eqq_1 = simplify( ddtpKEpq_1d - pKEpq_1 + pPEpq_1 - tau_1)
-eqq_2 = simplify( ddtpKEpq_2d - pKEpq_2 + pPEpq_2 - tau_2)
+syms kd kp 
+v_1 = kd*-q_1d + kp*(q_1d - q_1);
+tau_1 = simplify( ddtpKEpq_1d - pKEpq_1 + pPEpq_1);
+tau_1 = subs(tau_1,q_1dd,v_1)
+matlabFunction(tau_1, 'file', 'ControlTorque1');
+eqq_2 = simplify( ddtpKEpq_2d - pKEpq_2 + pPEpq_2) == 0 
+%tau_2 should be zero
+
+q_2dd = isolate(eqq_2,q_2dd)
+q_2dd
